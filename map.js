cd76d2df8538c1b7d172bc1360f544cd4b5783e0
soundwanders <soundwanders@gmail.com>
2021-08-19 22:52:09 -0400

    Shelter Map data table is functional, needs styling

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-let shelterMap = L.map('map-display').setView([43.241086332706374, -71.82000972872403] , 8);
+var shelterMap = L.map('map-display').setView([43.241086332706374, -71.82000972872403] , 8);
                 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -17,10 +17,10 @@ let iconOptions = {
  }
  
  // Creating a custom icon
-let smallIcon = L.icon(iconOptions);
+const smallIcon = L.icon(iconOptions);
 
 // Options for the marker
-let markerOptions = {
+const markerOptions = {
     title: this.value ,
     clickable: true ,
     draggable: false ,
--- mapTable.js
+++ mapTable.js
@@ -1,94 +1,128 @@
-let mapData = 
+let mapData =
     [
-        { location: "Bedford <br/>", name: "Animal Rescue League of New Hampshire <br/>", address: "545 NH-101, Bedford, NH 03110", 
-        longitude: 42.88017166872979, latitude: -71.57145002651751
+        { Location: "Bedford", Name: "Animal Rescue League of New Hampshire", Address: "545 NH-101, Bedford, NH 03110", 
+        Latitude: 42.88017166872979, Longitude: -71.57145002651751
         },
 
-        { location: "Charlestown <br/>", name: " River Valley Animal Protection <br/>", address: "60 Cummings Ave, Charlestown, NH 03603",
-        longitude: 43.25343420053334, latitude: -72.27105875717771
+        { Location: "Charlestown", Name: " River Valley Animal Protection", Address: "60 Cummings Ave, Charlestown, NH 03603",
+        Latitude: 43.25343420053334, Longitude: -72.27105875717771
         },
 
-        { location: "Chichester <br/>", name: "Live & Let Farm <br/>", address: "20 Paradice Ln, Chichester, NH 03258", 
-        longitude: 43.27309109525063, latitude: -71.61676265840609
+        { Location: "Chichester", Name: "Live & Let Farm", Address: "20 Paradice Ln, Chichester, NH 03258", 
+        Latitude: 43.27309109525063, Longitude: -71.61676265840609
         },
 
-        { location: "Concord <br/>", name: "Pope Memorial SPCA of Concord <br/>", address: "94 Silk Farm Rd, Concord, NH 03301", 
-        longitude: 43.241086332706374, latitude: -71.82000972872403
+        { Location: "Concord", Name: "Pope Memorial SPCA of Concord", Address: "94 Silk Farm Rd, Concord, NH 03301", 
+        Latitude: 43.241086332706374,Longitude: -71.82000972872403
         },
 
-        { location: "Conway <br/>", name: "Conway Area Humane Society <br/>", address: "223 E Main St, Conway, NH 03818", 
-        longitude: 43.994603572767254, latitude: -71.04689021327206
+        { Location: "Conway", Name: "Conway Area Humane Society", Address: "223 E Main St, Conway, NH 03818", 
+        Latitude: 43.994603572767254,Longitude: -71.04689021327206
         },
 
-        { location: "Derry <br/>", name: "The Greater Derry Humane Society <br/>", address: "11 Beaver Lake Rd, Derry, NH 03038", 
-        longitude: 42.92103589577785, latitude: -71.25009087535844
+        { Location: "Derry", Name: "The Greater Derry Humane Society", Address: "11 Beaver Lake Rd, Derry, NH 03038", 
+        Latitude: 42.92103589577785, Longitude: -71.25009087535844
         },
 
-        { location: "Dover <br/>", name: "Pope Memorial Humane Society - Cocheco Valley <br/>", address: "221 County Farm Rd, Dover, NH 03820", 
-        longitude: 43.23227695833639, latitude: -70.8630601671781
+        { Location: "Dover", Name: "Pope Memorial Humane Society - Cocheco Valley", Address: "221 County Farm Rd, Dover, NH 03820", 
+        Latitude: 43.23227695833639, Longitude: -70.8630601671781
         },
 
-        { location: "Enfield <br/>", name: "Upper Valley Humane Society <br/>", address: "300 Old Rte 10, Enfield, NH 03748",
-        longitude: 43.61734117845991, latitude: -72.08164451714268
+        { Location: "Enfield", Name: "Upper Valley Humane Society", Address: "300 Old Rte 10, Enfield, NH 03748",
+        Latitude: 43.61734117845991, Longitude: -72.08164451714268
         },
 
-        { location: "Franklin <br/>", name: "Franklin Animal Shelter <br/>", address: "19 Rescue Road, Franklin, NH 03235", 
-        longitude: 43.42856839976283, latitude: -71.58582048313339
+        { Location: "Franklin", Name: "Franklin Animal Shelter", Address: "19 Rescue Road, Franklin, NH 03235", 
+        Latitude: 43.42856839976283, Longitude: -71.58582048313339
         },
 
-        { location: "Hudson <br/>", name: "Happy Tails Pet Rescue <br/>", address: "14 Pine Rd, Hudson, NH 03051", 
-        longitude: 42.72227834132841, latitude: -71.34458524183644
+        { Location: "Hudson", Name: "Happy Tails Pet Rescue", Address: "14 Pine Rd, Hudson, NH 03051", 
+        Latitude: 42.72227834132841, Longitude: -71.34458524183644
         },
 
-        { location: "Laconia <br/>", name: "New Hampshire Humane Society <br/>", address: "1305 Meredith Center Rd, Laconia, NH 03246", 
-        longitude: 43.63099127848263, latitude: -71.36545040256439
+        { Location: "Laconia", Name: "New Hampshire Humane Society", Address: "1305 Meredith Center Rd, Laconia, NH 03246", 
+        Latitude: 43.63099127848263, Longitude: -71.36545040256439
         },
 
-        { location: "Littleton <br/>", name: "Second Chance Animal Rescue <br/>", address: "1517 Meadow St, Littleton, NH 03561", 
-        longitude: 44.311944563627286, latitude: -72.02325679904196
+        { Location: "Littleton", Name: "Second Chance Animal Rescue", Address: "1517 Meadow St, Littleton, NH 03561", 
+        Latitude: 44.311944563627286,Longitude: -72.02325679904196
         },
 
-        { location: "Manchester <br/>", name: "Manchester Animal Shelter <br/>", address: "490 Dunbarton Rd, Manchester, NH 03102", 
-        longitude: 43.597342526041565, latitude: -71.51698816320429
+        { Location: "Manchester", Name: "Manchester Animal Shelter", Address: "490 Dunbarton Rd, Manchester, NH 03102", 
+        Latitude: 43.597342526041565,Longitude: -71.51698816320429
         },
 
-        { location: "Nashua <br/>", name: "Humane Society for Greater Nashua <br/>", address: "24 Ferry Rd, Nashua, NH 03064", 
-        longitude: 42.79995651951563, latitude: -71.61334325859174
+        { Location: "Nashua", Name: "Humane Society for Greater Nashua", Address: "24 Ferry Rd, Nashua, NH 03064", 
+        Latitude: 42.79995651951563, Longitude: -71.61334325859174
         },
 
-        { location: "New Boston <br/>", name: "Canine Commitment <br/>", address: "733 Bedford Rd, New Boston, NH 03070", 
-        longitude: 42.98273072288184, latitude: -71.52338484097939
+        { Location: "New Boston", Name: "Canine Commitment", Address: "733 Bedford Rd, New Boston, NH 03070", 
+        Latitude: 42.98273072288184, Longitude: -71.52338484097939
         },
 
-        { location: "Northwood <br/>", name: "Mary's Dogs Rescue & Adoption <br/>", address: "984 1st New Hampshire Turnpike, Northwood, NH 03261", 
-        longitude:  43.2307150104135, latitude: -71.1631204070168
+        { Location: "Northwood", Name: "Mary's Dogs Rescue & Adoption", Address: "984 1st New Hampshire Turnpike, Northwood, NH 03261", 
+        Latitude:  43.2307150104135, Longitude: -71.1631204070168
         },
 
-        { location: "Ossipee <br/>", name: "Lakes Region Humane Society <br/>", address: "11 Old Rte 28, Ossipee, NH 03864", 
-        longitude: 43.701499940709354, latitude: -71.07464196054205
+        { Location: "Ossipee", Name: "Lakes Region Humane Society", Address: "11 Old Rte 28, Ossipee, NH 03864", 
+        Latitude: 43.701499940709354,Longitude: -71.07464196054205
         },
 
-        { location: "Salem <br/>", name: "Salem Animal Rescue League <br/>", address: "4 SARL Dr, Salem, NH 03079", 
-        longitude: 42.774256733036836, latitude: -71.14367773124076
-        },
-        { location:"Seabrook <br/>", name: "Cat Tales Rescue <br/>", address: "920 Lafayette Rd #201, Seabrook, NH 03874", 
-        longitude: 42.94488441193613, latitude: -70.78387548513204
-        },
-        { location: "Stratham <br/>", name: "New Hampshire SPCA <br/>", address: "104 Portsmouth Ave, Stratham, NH 03885", 
-        longitude: 43.03077429588942, latitude: -70.88549902029635
-        },
-        { location: "Sugar Hill <br/>", name: "Above the Notch Humane Society <br/>", address: "298 NH-18, Sugar Hill, NH 03586", 
-        longitude: 44.24835044609184, latitude: -71.72835566235251
-        },
-        { location: "Swanzey <br/>", name: "Monadnock Humane Society <br/>", address: "101 W Swanzey Rd, Swanzey, NH 03446" , 
-        longitude: 42.93483097189659, latitude: -72.14343359063717
+        { Location: "Salem", Name: "Salem Animal Rescue League", Address: "4 SARL Dr, Salem, NH 03079", 
+        Latitude: 42.774256733036836,Longitude: -71.14367773124076
         },
 
-        { location: "Weare <br/>", name: "Hearts And Tails Animal Alliance <br/>", address: "35 Daniels Rd, Weare, NH 03281", 
-        longitude: 43.07542973659716, latitude: -71.5501718718713
+        { Location:"Seabrook", Name: "Cat Tales Rescue", Address: "920 Lafayette Rd #201, Seabrook, NH 03874", 
+        Latitude: 42.94488441193613, Longitude: -70.78387548513204
         },
 
-        { location: "Weare <br/>", name: "We Are Animal Guardians <br/>", address: "15 Flanders Memorial Rd, Weare, NH 03281", 
-        longitude: 43.11613860902086, latitude: -71.86004564719458
+        { Location: "Stratham", Name: "New Hampshire SPCA", Address: "104 Portsmouth Ave, Stratham, NH 03885", 
+        Latitude: 43.03077429588942, Longitude: -70.88549902029635
+        },
+
+        { Location: "Sugar Hill", Name: "Above the Notch Humane Society", Address: "298 NH-18, Sugar Hill, NH 03586", 
+        Latitude: 44.24835044609184, Longitude: -71.72835566235251
+        },
+
+        { Location: "Swanzey", Name: "Monadnock Humane Society", Address: "101 W Swanzey Rd, Swanzey, NH 03446" , 
+        Latitude: 42.93483097189659, Longitude: -72.14343359063717
+        },
+
+        { Location: "Weare", Name: "Hearts And Tails Animal Alliance", Address: "35 Daniels Rd, Weare, NH 03281", 
+        Latitude: 43.07542973659716, Longitude: -71.5501718718713
+        },
+
+        { Location: "Weare", Name: "We Are Animal Guardians", Address: "15 Flanders Memorial Rd, Weare, NH 03281", 
+        Latitude: 43.11613860902086, Longitude: -71.86004564719458
         },
     ];
+
+    
+    const dataTableHead = (table, data) => {
+        let tableHeader = table.createTHead();
+        let row = tableHeader.insertRow();
+        for (let key of data) {
+            let th = document.createElement("th");
+            let text = document.createTextNode(key);
+            th.appendChild(text);
+            row.appendChild(th);
+        }
+    };
+
+    const generateTable = (table, data) => {
+        for (let element of data) {
+            let row = table.insertRow();
+            for (key in element) {
+                let cell = row.insertCell();
+                let tableText = document.createTextNode(element[key]);
+                cell.appendChild(tableText);
+            }
+        }
+    }
+
+    const table = document.querySelector("table");
+    let data = Object.keys(mapData[0]);
+
+
+    generateTable(table, mapData);
+    dataTableHead(table, data);
