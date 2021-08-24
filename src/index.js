3f87818ea152ecd42df7d423ac0b02916a285190
soundwanders <soundwanders@gmail.com>
2021-08-23 20:07:26 -0400

    swap var => const

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-var style = require('./index.css');
-var leafletMap  = require('./map.js');
-var mapTable = require('./mapTable.js');
-var dataValidation = require('./dataValidation.js');
+const style = require('./index.css');
+const leafletMap  = require('./map.js');
+const mapTable = require('./mapTable.js');
+const dataValidation = require('./dataValidation.js');
 
 style;
 leafletMap();
